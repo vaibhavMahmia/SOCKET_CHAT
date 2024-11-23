34b6bb9190537b8128e2db71058b0410beb474bb
vaibhavM <vaibhav@kali.kali>
2024-11-23 21:09:54 +0530

    Complete client side design part

---
+++ client/src/components/sidebar/LogoutButton.jsx
@@ -0,0 +1,9 @@
+import { RiLogoutBoxLine } from "react-icons/ri";
+
+const LogoutButton = () => {
+  return (
+    <div className='mt-auto'><RiLogoutBoxLine className="w-6 h-6 text-white cursor-pointer"/></div>
+  )
+}
+
+export default LogoutButton
---
+++ client/src/pages/signup/Signup.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import GenderCheckBox from './GenderCheckBox'
+
+const Signup = () => {
+  return (
+    <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+        <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+            <h1 className='text-3xl font-semibold text-center text-gray-300'>
+                SignUp <span className='text-blue-500'>ChatApp</span>
+            </h1>
+            <form>
+                <div>
+                    <label className='label p-2'>
+                        <span className='text-base label-text'>FullName</span>
+                    </label>
+                    <input type="text" placeholder='Enter FullName' className='w-full input input-bordered h-10' />
+                </div>
+                <div>
+                    <label className='label p-2'>
+                        <span className='text-base label-text'>UserName</span>
+                    </label>
+                    <input type="text" placeholder='Enter UserName' className='w-full input input-bordered h-10' />
+                </div>
+                <div>
+                    <label className='label p-2'>
+                        <span className='text-base label-text'>Password</span>
+                    </label>
+                    <input type="password" placeholder='Enter Password' className='w-full input input-bordered h-10' />
+                </div>
+                <div>
+                    <label className='label p-2'>
+                        <span className='text-base label-text'>ConfirmPassword</span>
+                    </label>
+                    <input type="password" placeholder='Enter Password Again' className='w-full input input-bordered h-10' />
+                </div>
+                <GenderCheckBox/>
+                <a href="#" className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block'>
+                    Already have an account?
+                </a>
+                <div>
+                    <button className='btn btn-block btn-sm mt-2'>SignUp</button>
+                </div>
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default Signup
