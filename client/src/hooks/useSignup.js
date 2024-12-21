1e8db043035a74838dc16653c3a8054f2122e659
vaibhavM <vaibhav@kali.kali>
2024-12-22 00:53:15 +0530

    Complete context API for signup in frotend.

--- client/src/hooks/useSignup.js
+++ client/src/hooks/useSignup.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import toast from "react-hot-toast";
+import { useAuthContext } from "../context/AuthContext";
 
 const handleInputErrors = ({ fullName, username, password, confirmPassword, gender }) => {
     if(!fullName || !username || !password || !confirmPassword || !gender){
@@ -19,6 +20,7 @@ const handleInputErrors = ({ fullName, username, password, confirmPassword, gend
 
 const useSignup = () => {
   const [loading, setLoading] = useState(false);
+  const { authUser, setAuthUser } = useAuthContext();
   const signup = async ({ fullName, username, password, confirmPassword, gender }) => {
     const success = handleInputErrors({ fullName, username, password, confirmPassword, gender });
     if(!success) return;
@@ -32,7 +34,11 @@ const useSignup = () => {
 
         const data = await res.json();
         if(data.error)
-            throw new Error(data.error)
+            throw new Error(data.error);
+        //loclstorage
+        localStorage.setItem('chat-user', JSON.stringify(data));
+        //context
+        setAuthUser(data);
     } catch (error) {
         toast.error(error.message);
     } finally {
