088643c3769c9f2a5f90ad2dd4c165f907fd7b4e
vaibhavM <vaibhav@kali.kali>
2025-01-01 22:29:18 +0530

    updated correct socket url

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -14,7 +14,7 @@ export const SocketContextProvider = ({ children }) => {
     const { authUser } = useAuthContext();
     useEffect(() => {
         if(authUser){
-            const socket = io("https://chat-app-yt.onrender.com",{
+            const socket = io("https://socket-chat-vaibhavm.onrender.com",{
 				query: {
 					userId: authUser._id
 				}
